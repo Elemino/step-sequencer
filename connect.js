69c4ae5c571d6b44172b6de33511b9e0416c7fa7
Elemino <elemshalit@gmail.com>
2018-04-14 23:17:28 -0700

    Schedule a repeat

--- connect.js
+++ connect.js
@@ -7,3 +7,14 @@ synth.toMaster();
 
 //synth.triggerAttackRelease('C4', '8n')
 
+synth.triggerAttackRelease('c4', '8n')
+const notes [
+  'c4', 'E4', 'G4',
+  'C5', 'E5', 'G5',
+];
+
+var index = 0;
+
+Tone.Transport.scheduleRepeat(tine => {
+  repeat(time);
+}, '8n');
