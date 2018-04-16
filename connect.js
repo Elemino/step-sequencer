f4a4c0c90b621f973a294131119e93e2e5cfc37a
Elemino <elemshalit@gmail.com>
2018-04-16 12:12:55 -0700

    Add gain and connect synth

--- connect.js
+++ connect.js
@@ -10,15 +10,18 @@ synths[0].oscillator.type = 'triangle';
 synths[1].oscillator.type = 'sine';
 synths[2].oscillator.type = 'sawtooth';
 
+const gain = new Tone.gain(0.5);
+gain.toMaster();
 
-synths.forEach(synth => synth.toMaster());
+synths.forEach(synth => synth.connect(gain));
 
 //const $rows = document.body.querySelectorAll('div' > div'),
+
   notes = ['G5', 'E4', 'C3'];
 let index = 0;
 
 Tone.Transport.scheduleRepeat(repeat, '8n');
-
+Tone.Transport.start();
 
 function repeat(time) {
  let step = % 8;
