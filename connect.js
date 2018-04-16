fdc0dc00a26459ea37a03fadd347119f9378d12f
Elemino <elemshalit@gmail.com>
2018-04-15 17:17:14 -0700

    Find input for row

--- connect.js
+++ connect.js
@@ -16,16 +16,17 @@ synths.forEach(synth => synth.toMaster());
 const $rows = document.body.querySelectorAll('div' > div');
 
 Tone.Transport.scheduleRepeat(repeat, '8n');
-
-  let index = 0;
+let index = 0;
 
 function repeat(time) {
 
   for (let i = 0; i < $rows.length; i++) {
     let synth = synth[i],
-        $row = $rows[]
-        ;
+        $row = $rows[i],
+        $input =$rows.querySelector(`input:nth-child(${step + 1})`);
+        if($input.checked) synth.triggerAttackRelease(note, '8n', time);
   }
+  index++;
 }
 
 
