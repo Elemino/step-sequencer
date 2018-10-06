a02048e2e126f4f18316edba44e3d5961744f421
Elemino <elemshalit@gmail.com>
2018-10-05 22:31:58 -0700

    Remove clutter and add coma on repeat

--- connect.js
+++ connect.js
@@ -27,41 +27,9 @@ function repeat(time) {
  let step = % 8;
  for (let i = 0; i < $rows.length; i++) {
     let synth = synth[i],
-        note = notes[i]
+        note = notes[i],
         $row = $rows[i],
         $input =$rows.querySelector(`input:nth-child(${step + 1})`);
         if ($input.checked) synth.triggerAttackRelease(note, '8n', time);
   }
   index++;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var synth = new Tone.Synth().toMaster()
-//const synth = new Tone.synth();
-//synth.toMaster();
-//synth.triggerAttackRelease('C4', '8n')
-//synth.triggerAttackRelease('c4', '8n')
-//const notes [
-//  'c4', 'E4', 'G4',
-//  'C5', 'E5', 'G5',
-//];
-//var index = 0;
-//Tone.Transport.scheduleRepeat(tine => {
-//  repeat(time);
-//}, '8n');
