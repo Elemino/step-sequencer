b566ded533e7b88828406166f8910d5bd64f1258
Elemino <elemshalit@gmail.com>
2018-10-08 17:54:09 -0700

    Add record.js

--- connect.js
+++ connect.js
@@ -34,3 +34,5 @@ function repeat(time) {
   }
  }
   index++;
+
+  
---
+++ record.js
@@ -0,0 +1,11 @@
+const audio = document.querySelector('audio');
+const synth = new Tone.Synth();
+const obj = Tone.context;
+
+synth.toMaster();
+
+const notes = 'CDEFGAB'. split('').map(n => `${n}4`);
+let note = 0;
+Tone.Transport.scheduleRepeat(time => {
+
+});
